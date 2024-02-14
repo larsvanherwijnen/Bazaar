32446a2bf71dff67a05ccdccc89b37cdf9360f0c
PeterV293 <p.venner@student.avans.nl>
2024-02-14 21:59:06 +0100

    Icon position improved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,8 +25,7 @@
             <label>
                 <input type="text" name="query" placeholder="Search..." class="w-full outline-none focus:outline-none">
             </label>
-            <button type="submit" class="bg-blue-500 text-white rounded-full px-4 py-2 ml-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
+            <button type="submit" class="bg-blue-500 text-white rounded-full px-4 py-2 ml-auto">                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
                     <path fill-rule="evenodd"
                           d="M8.5 16a7.5 7.5 0 100-15 7.5 7.5 0 000 15zm6.854-2.146l4.647 4.646-1.708 1.708-4.646-4.647a6.5 6.5 0 111.707-1.707zM15 9a6 6 0 11-12 0 6 6 0 0112 0z"
                           clip-rule="evenodd"/>
