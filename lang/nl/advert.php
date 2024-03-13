d047a05104443758ae3adee26a6c26225a28e37f
PeterV293 <p.venner@student.avans.nl>
2024-03-13 13:15:29 +0100

    Added more to lang

--- lang/nl/advert.php
+++ lang/nl/advert.php
@@ -11,4 +11,7 @@ return [
     'used' => 'gebruikt',
     'Click_or_drag_to_upload_a_photo' => 'Klik of sleep om een foto te uploaden',
     'price' => 'Prijs',
+    'starting_price' => 'Startprijs',
+    'start_date' => 'Startdatum',
+    'end_date' => 'Einddatum',
 ];
