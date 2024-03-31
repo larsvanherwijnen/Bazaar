c1fe201162559a60fab8f357799f2a3081a8d401
PeterV293 <PeterV293@users.noreply.github.com>
2024-03-31 15:09:06 +0000

    Fix styling

--- app/Http/Controllers/AdvertController.php
+++ app/Http/Controllers/AdvertController.php
@@ -19,22 +19,22 @@ class AdvertController extends Controller
     /**
      * Display the specified resource.
      */
- public function show(Advert $advert): View
-{
-    $user = auth()->user();
-    $reviews = $advert->reviews;
-    $userReviews = $advert->user->reviews;
+    public function show(Advert $advert): View
+    {
+        $user = auth()->user();
+        $reviews = $advert->reviews;
+        $userReviews = $advert->user->reviews;
 
-    $data = [
-        'advert' => $advert,
-        'reviewsCount' => $userReviews->count(),
-        'averageRating' => $userReviews->avg('rating'),
-        'reviewsCountRental' => $reviews->count(),
-        'averageRatingRental' => $reviews->avg('rating'),
-        'showReviewButton' => $reviews->whereNotNull('comment')->count() > 0,
-        'showReviewCreateButton' => $user ? $reviews->where('reviewer_id', $user->id)->isEmpty() : false,        'reviews' => $reviews->whereNotNull('comment')
-    ];
+        $data = [
+            'advert' => $advert,
+            'reviewsCount' => $userReviews->count(),
+            'averageRating' => $userReviews->avg('rating'),
+            'reviewsCountRental' => $reviews->count(),
+            'averageRatingRental' => $reviews->avg('rating'),
+            'showReviewButton' => $reviews->whereNotNull('comment')->count() > 0,
+            'showReviewCreateButton' => $user ? $reviews->where('reviewer_id', $user->id)->isEmpty() : false,        'reviews' => $reviews->whereNotNull('comment'),
+        ];
 
-    return view('advert.show')->with($data);
-}
+        return view('advert.show')->with($data);
+    }
 }
--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -8,35 +8,33 @@ use App\Models\Review;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\View\View;
 
 class ReviewController extends Controller
 {
     public function store(ReviewRequest $request): RedirectResponse
-{
-    $user = Auth::user();
-    /** @var User|null $reviewedUser */
-    $reviewedUser = $request->has('user_id') ? User::find($request->user_id) : null;
-    /** @var Advert|null $reviewedAdvert */
-    $reviewedAdvert = $request->has('advert_id') ? Advert::find($request->advert_id) : null;
+    {
+        $user = Auth::user();
+        /** @var User|null $reviewedUser */
+        $reviewedUser = $request->has('user_id') ? User::find($request->user_id) : null;
+        /** @var Advert|null $reviewedAdvert */
+        $reviewedAdvert = $request->has('advert_id') ? Advert::find($request->advert_id) : null;
 
-    $existingReview = $reviewedUser ?
-        $reviewedUser->reviews->firstWhere('reviewer_id', $user->id) :
-        $reviewedAdvert->reviews->firstWhere('reviewer_id', $user->id);
-    if ($existingReview || ($reviewedUser && $user->id == $reviewedUser->id)) {
-        return back();
+        $existingReview = $reviewedUser ?
+            $reviewedUser->reviews->firstWhere('reviewer_id', $user->id) :
+            $reviewedAdvert->reviews->firstWhere('reviewer_id', $user->id);
+        if ($existingReview || ($reviewedUser && $user->id == $reviewedUser->id)) {
+            return back();
+        }
+
+        if ($request->validated()) {
+            $reviewData = $request->validated() + ['reviewer_id' => $user->id];
+            Review::create($reviewData);
+        }
+
+        return $reviewedUser ?
+            redirect()->route('profile', ['url' => $reviewedUser->url]) :
+            redirect()->route('adverts.show', ['advert' => $reviewedAdvert->id]);
     }
-
-    if ($request->validated()) {
-        $reviewData = $request->validated() + ['reviewer_id' => $user->id];
-        Review::create($reviewData);
-    }
-
-    return $reviewedUser ?
-        redirect()->route('profile', ['url' => $reviewedUser->url]) :
-        redirect()->route('adverts.show', ['advert' => $reviewedAdvert->id]);
-}
 
     public function destroy(string $id): RedirectResponse
     {
