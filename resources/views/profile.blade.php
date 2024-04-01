e57d350232952b9be640b04d17a5350a9f41a9b7
PeterV293 <p.venner@student.avans.nl>
2024-04-01 20:54:49 +0200

    Added 5 random ads

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,9 +3,13 @@
 @section('content')
     @include('partials.user_header', ['user' => $user])
     <div class="container mx-auto pt-8 space-y-4">
-        <div class="flex flex-col px-4 bg-gray-300">
-            <h2 class="font-bold uppercase text-2xl">{{__('global.featured')}}</h2>
-            carousel met de uitgelichte advertenties (max 5 ofz )
+        <div class="flex flex-row flex-wrap justify-center bg-gray-300">
+            <h2 class="font-bold uppercase text-2xl w-full mb-4 text-center">{{__('global.featured')}}</h2>
+            <div class="flex flex-row flex-wrap justify-center">
+                @foreach($adverts as $advert)
+                        @include('partials.advert_card_small', ['advert' => $advert])
+                @endforeach
+            </div>
         </div>
         <livewire:adverts :user="$user"/>
     </div>
