16dcef086d4be7fdf28210471bd5805e6d8bae28
Lars van Herwijnen <lars.druten@gmail.com>
2024-04-01 14:20:11 +0200

    Fix UI

--- resources/views/advert/show.blade.php
+++ resources/views/advert/show.blade.php
@@ -8,6 +8,7 @@
                 <div class="bg-white p-5 rounded w-2/3">
                     @include('partials.advert.advert', $advert)
                 </div>
+
                 <div class="flex flex-col space-y-5 w-1/3">
                     <div class="bg-white p-5 rounded flex-col">
                         @include('partials.advert.seller', $advert)
@@ -17,8 +18,7 @@
                             <livewire:bidding :advert="$advert"/>
                         @elseif($advert->type == App\Enum\AdvertType::RENTAL)
                             <livewire:booking :advert="$advert"/>
-                        @include('partials.advert_review', ['averageRatingRental' => $averageRatingRental, 'reviewsCountRental' => $reviewsCountRental, 'showReviewCreateButton' => $showReviewCreateButton, 'showReviewButton' => $showReviewButton, ])
-
+                            @include('partials.advert_review', ['averageRatingRental' => $averageRatingRental, 'reviewsCountRental' => $reviewsCountRental, 'showReviewCreateButton' => $showReviewCreateButton, 'showReviewButton' => $showReviewButton, ])
                         @elseif($advert->type == App\Enum\AdvertType::SALE)
                             @if(!$advert->bought_by)
                                 <form action="{{ route('adverts.sellBuy', ['advert' => $advert->id]) }}" method="POST">
--- resources/views/partials/advert/advert.blade.php
+++ resources/views/partials/advert/advert.blade.php
@@ -13,53 +13,53 @@
         </div>
     @endif
     <div class=" pb-5 flex justify-between">
-            <h2 class="text-xl font-bold text-gray-900">{{ $advert->title }}</h2>
-             <livewire:favorite :advert="$advert"/>
-        </div>
-        <div x-data="{ mainImage: '' }"
-             x-init="mainImage = '{{ $advert->advertImages->first() ? Storage::url('images/' . $advert->advertImages->first()->path) : Storage::url('images/img.png') }}'"
-             class="flex space-x-5">
-            <div class="flex flex-col-reverse">
-                <div class="order-2">
-                    <div class="max-w-96 overflow-hidden rounded-lg">
-                        <img x-bind:src="mainImage" class="h-full w-full max-w-full object-cover" alt=""/>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full">
-                <div class="flex flex-row items-start">
-                    @foreach($advert->advertImages as $image)
-                        <button type="button"
-                                @click="mainImage = '{{ Storage::url('images/' . $image->path) }}'"
-                                class="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-transparent text-center">
-                            <img class="h-full w-full object-cover"
-                                 src="{{ Storage::url('images/' . $image->path) }}" alt=""/>
-                        </button>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-        @if($advert->type == AdvertType::BIDDING && $advert->price != null)
-            <span class="text-xl font-bold text-gray-900">€{{ $advert->price }}</span>
-        @elseif($advert->type == AdvertType::BIDDING)
-            <span class="text-xl font-bold text-gray-900">{{ __('advert.bidding') }}</span>
-        @elseif($advert->type == AdvertType::AUCTION)
-            <span class="text-xl font-bold text-gray-900">{{ __('advert.auction') }}</span>
-        @elseif($advert->type == AdvertType::RENTAL)
-            <div class="flex flex-col">
-                <span class="text-xl font-bold text-gray-900">{{ __('advert.rental') }}</span>
-                <span class="text-sm text-gray-500">€{{ $advert->price }}/{{ __('advert.per_day') }}</span>
-            </div>
-        @else
-            <span class="text-xl font-bold text-gray-900">€{{ $advert->price }}</span>
-        @endif
-
+        <h2 class="text-xl font-bold text-gray-900">{{ $advert->title }}</h2>
+        <livewire:favorite :advert="$advert"/>
     </div>
+    <div x-data="{ mainImage: '' }"
+         x-init="mainImage = '{{ $advert->advertImages->first() ? Storage::url('images/' . $advert->advertImages->first()->path) : Storage::url('images/img.png') }}'"
+         class="flex space-x-5">
+        <div class="flex flex-col-reverse">
+            <div class="order-2">
+                <div class="max-w-96 overflow-hidden rounded-lg">
+                    <img x-bind:src="mainImage" class="h-full w-full max-w-full object-cover" alt=""/>
+                </div>
+            </div>
+        </div>
+        <div class="w-full">
+            <div class="flex flex-row items-start">
+                @foreach($advert->advertImages as $image)
+                    <button type="button"
+                            @click="mainImage = '{{ Storage::url('images/' . $image->path) }}'"
+                            class="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-transparent text-center">
+                        <img class="h-full w-full object-cover"
+                             src="{{ Storage::url('images/' . $image->path) }}" alt=""/>
+                    </button>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    @if($advert->type == AdvertType::BIDDING && $advert->price != null)
+        <span class="text-xl font-bold text-gray-900">€{{ $advert->price }}</span>
+    @elseif($advert->type == AdvertType::BIDDING)
+        <span class="text-xl font-bold text-gray-900">{{ __('advert.bidding') }}</span>
+    @elseif($advert->type == AdvertType::AUCTION)
+        <span class="text-xl font-bold text-gray-900">{{ __('advert.auction') }}</span>
+    @elseif($advert->type == AdvertType::RENTAL)
+        <div class="flex flex-col">
+            <span class="text-xl font-bold text-gray-900">{{ __('advert.rental') }}</span>
+            <span class="text-sm text-gray-500">€{{ $advert->price }}/{{ __('advert.per_day') }}</span>
+        </div>
+    @else
+        <span class="text-xl font-bold text-gray-900">€{{ $advert->price }}</span>
+    @endif
     <div class="flex flex-col">
         <small class="font-bold text-gray-900 text-xl">{{__('global.description')}}</small>
         {{ $advert->description}}
     </div>
     <div class="mt-8 space-y-2">
-        {{--            <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(64)->generate(route('adverts.show', $advert))) !!} ">--}}
+                    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(64)->generate(route('adverts.show', $advert))) !!} ">
     </div>
 </div>
+
+
